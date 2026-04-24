ee6f812ec247a6db9f0c88fca4e54617b6f20e6c
taket <taketen.kei.shi@gmail.com>
2026-04-24 11:44:06 +0900

    UI feature - app icon

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
+#include <QIcon>
 
 #include "app/mainwindow/MainWindow.h"
 
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
+  app.setWindowIcon(QIcon(":/icons/app_icon.ico"));
   app::mainwindow::MainWindow window;
   window.show();
   return QApplication::exec();
